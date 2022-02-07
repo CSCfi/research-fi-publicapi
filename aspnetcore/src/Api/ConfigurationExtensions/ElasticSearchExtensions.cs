2aedc56340bbefc56b806c44a57fe76843a1ced9
Jari Randelin <jari.randelin@gmail.com>
2022-02-07 08:09:57 +0200

    Added ElasticSearch basic auth configuration

--- aspnetcore/src/Api/ConfigurationExtensions/ElasticSearchExtensions.cs
+++ aspnetcore/src/Api/ConfigurationExtensions/ElasticSearchExtensions.cs
@@ -6,9 +6,15 @@ namespace Api.ConfigurationExtensions
     {
         public static void AddElasticSearch(this IServiceCollection services, IConfiguration configuration)
         {
-            var elasticSearchClusterUrl = configuration["ELASTICSEARCH_URL"] ?? throw new InvalidOperationException("ElasticSearch url missing.");
-            var connectionSettings = new ConnectionSettings(new Uri(elasticSearchClusterUrl));
+            var elasticSearchClusterUrl = configuration["ELASTICSEARCH:URL"] ?? throw new InvalidOperationException("ElasticSearch url missing.");
+            var elasticSearchUserName = configuration["ELASTICSEARCH:USERNAME"] ?? throw new InvalidOperationException("ElasticSearch username missing.");
+            var elasticSearchPassword = configuration["ELASTICSEARCH:PASSWORD"] ?? throw new InvalidOperationException("ElasticSearch password missing.");
+            
+            var connectionSettings = new ConnectionSettings(new Uri(elasticSearchClusterUrl))
+                .BasicAuthentication(elasticSearchUserName, elasticSearchPassword);
+            
             var elasticClient = new ElasticClient(connectionSettings);
+            
             services.AddSingleton<IElasticClient>(elasticClient);
         }
     }
