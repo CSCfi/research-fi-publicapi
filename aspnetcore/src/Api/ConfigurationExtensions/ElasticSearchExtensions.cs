873cabae4eee215b9d8283c636fb7d8124aca7f7
Jari Randelin <jari.randelin@gmail.com>
2022-02-07 08:00:54 +0200

    Changed ElasticSearch config key to be more k8s friendly

--- aspnetcore/src/Api/ConfigurationExtensions/ElasticSearchExtensions.cs
+++ aspnetcore/src/Api/ConfigurationExtensions/ElasticSearchExtensions.cs
@@ -6,7 +6,7 @@ namespace Api.ConfigurationExtensions
     {
         public static void AddElasticSearch(this IServiceCollection services, IConfiguration configuration)
         {
-            var elasticSearchClusterUrl = configuration["elasticsearch:url"] ?? throw new InvalidOperationException("ElasticSearch url missing.");
+            var elasticSearchClusterUrl = configuration["ELASTICSEARCH_URL"] ?? throw new InvalidOperationException("ElasticSearch url missing.");
             var connectionSettings = new ConnectionSettings(new Uri(elasticSearchClusterUrl));
             var elasticClient = new ElasticClient(connectionSettings);
             services.AddSingleton<IElasticClient>(elasticClient);
