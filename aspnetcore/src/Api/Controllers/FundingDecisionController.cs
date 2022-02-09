b195e2c4ac612aea42b8079dc94adab0a4713c26
Jari Randelin <jari.randelin@gmail.com>
2022-02-09 08:45:14 +0200

    Added FundingDecisionController

---
+++ aspnetcore/src/Api/Controllers/FundingDecisionController.cs
@@ -0,0 +1,33 @@
+﻿using Api.Models;
+using Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FundingDecisionController : ControllerBase
+    {
+        private readonly ILogger<FundingDecisionController> _logger;
+        private readonly ISearchService _searchService;
+
+        public FundingDecisionController(
+            ILogger<FundingDecisionController> logger,
+            ISearchService searchService)
+        {
+            _logger = logger;
+            _searchService = searchService;
+        }
+
+        /// <summary>
+        /// Hae rahoituspäätöksiä
+        /// </summary>
+        /// <param name="publicationName">Julkaisun nimi</param>
+        /// <returns></returns>
+        [HttpGet(Name = "GetFundingDecision")]
+        public IEnumerable<FundingDecision> Get(string publicationName)
+        {
+            return _searchService.Search<FundingDecision>(publicationName);
+        }
+    }
+}
