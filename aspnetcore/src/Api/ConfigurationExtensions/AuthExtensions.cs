d1de8ac7fad7525b02f20e09596ecaa2f055e603
Jari Randelin <jari.randelin@gmail.com>
2022-02-16 13:49:12 +0200

    Added TokenValidationParameters

--- aspnetcore/src/Api/ConfigurationExtensions/AuthExtensions.cs
+++ aspnetcore/src/Api/ConfigurationExtensions/AuthExtensions.cs
@@ -10,9 +10,19 @@ namespace Api.ConfigurationExtensions
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
-                    options.Audience = configuration.GetSection("keycloak")["audience"];
                     options.Authority = configuration.GetSection("keycloak")["authority"];
+                    options.Audience = configuration.GetSection("keycloak")["audience"];
                     options.MetadataAddress = configuration.GetSection("keycloak")["metadataaddress"];
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = configuration.GetSection("keycloak")["authority"],
+                        ValidAudiences = new[] { configuration.GetSection("keycloak")["audience"] },
+                        ValidateIssuerSigningKey = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
                 });
 
             services.AddAuthorization(options =>
