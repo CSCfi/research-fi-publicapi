931437ec7bf542be6413063d5647f9700844e72c
Jari Randelin <jari.randelin@gmail.com>
2022-02-21 14:04:58 +0200

    Added POST endpoint for FundingCalls.

--- aspnetcore/src/Api/Controllers/FundingCallController.cs
+++ aspnetcore/src/Api/Controllers/FundingCallController.cs
@@ -1,4 +1,6 @@
-﻿using Api.Models.FundingCall;
+﻿using Api.DatabaseContext;
+using Api.Models.Entities;
+using Api.Models.FundingCall;
 using Api.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -13,13 +15,16 @@ namespace Api.Controllers
 
         private readonly ILogger<FundingCallController> _logger;
         private readonly ISearchService<FundingCallSearchParameters,FundingCall> _searchService;
+        private readonly ApiDbContext _dbContext;
 
         public FundingCallController(
             ILogger<FundingCallController> logger,
-            ISearchService<FundingCallSearchParameters,FundingCall> searchService)
+            ISearchService<FundingCallSearchParameters,FundingCall> searchService,
+            ApiDbContext dbContext)
         {
             _logger = logger;
             _searchService = searchService;
+            _dbContext = dbContext;
         }
 
         /// <summary>
@@ -31,6 +36,20 @@ namespace Api.Controllers
         public IEnumerable<FundingCall> Get([FromQuery]FundingCallSearchParameters searchParameters)
         {
             return _searchService.Search(searchParameters);
+        }
+
+        [HttpPost(Name = "PostFundingCall")]
+        public async Task Post(FundingCall fundingCall)
+        {
+            await _dbContext
+                .Set<DimCallProgramme>()
+                .AddAsync(new DimCallProgramme
+                {
+                    NameFi = "test fi",
+                    SourceId = "api",
+                    DimRegisteredDataSourceId = -1
+                });
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
