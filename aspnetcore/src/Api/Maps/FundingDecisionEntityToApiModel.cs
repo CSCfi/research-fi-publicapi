96467e4cafdd5f3b8c6e98967b34e94a0b14cae0
Jari Randelin <jari.randelin@gmail.com>
2022-03-11 14:49:55 +0200

    Added mapping for DimFundingDecision -> FundingDecision

---
+++ aspnetcore/src/Api/Maps/FundingDecisionEntityToApiModel.cs
@@ -0,0 +1,21 @@
+﻿using Api.Models.Entities;
+using Api.Models.FundingDecision;
+
+namespace Api.Maps
+{
+    public class FundingDecisionEntityToApiModel : IMapper<DimFundingDecision, FundingDecision>
+    {
+        public FundingDecision Map(DimFundingDecision input)
+        {
+            return new FundingDecision
+            {
+                NameFi = input.NameFi,
+                NameSv = input.NameSv,
+                NameEn = input.NameEn,
+                DescriptionFi = input.DescriptionFi,
+                DescriptionSv = input.DescriptionSv,
+                DescriptionEn = input.DescriptionEn
+            };
+        }
+    }
+}
