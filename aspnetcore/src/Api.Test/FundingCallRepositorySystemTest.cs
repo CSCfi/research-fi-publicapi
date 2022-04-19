aa9a1597f6e3b675cce2fdd6f1b0addd87a579d8
Jari Randelin <jari.randelin@gmail.com>
2022-04-19 13:49:27 +0300

    Added system tests for Infrastructures

---
+++ aspnetcore/src/Api.Test/FundingCallRepositorySystemTest.cs
@@ -0,0 +1,33 @@
+﻿using Api.DataAccess.Repositories;
+using Api.Models.FundingCall;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Test
+{
+    public class FundingCallRepositorySystemTest: IClassFixture<TestWebApplicationFactory<Program>>
+    {
+        private readonly IIndexRepository<FundingCall> _repository;
+
+        public FundingCallRepositorySystemTest(TestWebApplicationFactory<Program> factory)
+        {
+            var scope = factory.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            _repository = scope.ServiceProvider.GetRequiredService<IIndexRepository<FundingCall>>();
+        }
+
+        [Fact]
+        public async Task Repo_ShouldReturn_Something()
+        {
+            var entities = await _repository.GetAllAsync()
+                .Where(x => x.NameFi == "FIRI 2019: Suomen tiekartalla olevat infrastruktuurit, kv-jäsenyydet 2019")
+                .ToListAsync();
+
+            // Assert
+            entities.Should().NotBeEmpty();
+
+        }
+    }
+}
---
+++ aspnetcore/src/Api.Test/FundingDecisionRepositorySystemTest.cs
@@ -0,0 +1,45 @@
+﻿using Api.DataAccess.Repositories;
+using Api.Models.Entities;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Test
+{
+    public class FundingDecisionRepositorySystemTest: IClassFixture<TestWebApplicationFactory<Program>>
+    {
+        private readonly IGenericRepository<DimFundingDecision> _repository;
+
+        public FundingDecisionRepositorySystemTest(TestWebApplicationFactory<Program> factory)
+        {
+            var scope = factory.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            _repository = scope.ServiceProvider.GetRequiredService<IFundingDecisionRepository>();
+        }
+
+        [Fact]
+        public async Task Repo_ShouldReturn_Something()
+        {
+            var entities = _repository.GetAll()
+                //.Where(x => x.NameEn != null && x.NameEn.Contains("raha", StringComparison.CurrentCultureIgnoreCase))
+                //.Where(x => x.DimWebLinks != null && x.DimWebLinks.Any(l => l.LinkType == "ApplicationURL"))
+                //.Where(x => x.DimDateIdDueNavigation != null)
+                //.Where(x => x.DimDateIdOpen == -1)
+                .ToList();
+
+            // Assert
+            entities.SingleOrDefault(e => e.Id == -1).Should().BeNull();
+            entities.Should().NotContain(e => e.DimTypeOfFunding == null);
+            entities.Should().NotContain(e => e.DimTypeOfFunding != null && e.DimTypeOfFunding.TypeId == "62");
+            entities.Should().NotContain(e => e.DimTypeOfFunding != null && e.DimTypeOfFunding.TypeId == "66");
+            entities.Should().NotContain(e => e.DimTypeOfFunding != null && e.DimTypeOfFunding.TypeId == "69");
+
+            entities
+                .Should().HaveCount(36699);
+
+        }
+
+    }
+}
