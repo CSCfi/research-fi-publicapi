49208da3f2c69dc4edcda1428e4bdc1f738ce0ea
Jari Randelin <jari.randelin@gmail.com>
2022-06-23 13:32:36 +0300

    Merge fix

--- aspnetcore/src/ElasticSearchIndexer/Indexer.cs
+++ aspnetcore/src/ElasticSearchIndexer/Indexer.cs
@@ -85,13 +85,13 @@ namespace ElasticSearchIndexer
                 _logger.LogInformation("Getting '{entityType}' entities from the database.", type.Name);
 
                 var indexModels = await repository.GetAllAsync().ToListAsync();
-                _logger.LogInformation("Got {count} '{entityType}' entities from the database. {stopWatch}", indexModels.Count, type.Name, _stopWatch.Elapsed);
+                _logger.LogInformation("Got {count} '{entityType}' entities from the database. {stopWatch}", indexModels.Count, type.Name, stopWatch.Elapsed);
                 var finalized = repository.PerformInMemoryOperations(indexModels);
 
                 _logger.LogInformation("Indexing '{indexName}' to ElasticSearch..", indexName);
 
                 await _indexService.IndexAsync(indexName, finalized, type);
-                _logger.LogInformation("Index '{indexName}' created. {stopWatch}", indexName, _stopWatch.Elapsed);
+                _logger.LogInformation("Index '{indexName}' created. {stopWatch}", indexName, stopWatch.Elapsed);
                 return (indexName, true, stopWatch.Elapsed);
             }
             catch (Exception ex)
