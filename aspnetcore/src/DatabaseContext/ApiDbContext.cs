106cb0ce3ab165ebc7058b0ef61aa2d44c7e751d
Timo Särkikoski <sarkikos@gmail.com>
2023-09-15 10:41:19 +0300

    Remove connectionstring from generated DB context. Handle null value in publication license.

--- aspnetcore/src/DatabaseContext/ApiDbContext.cs
+++ aspnetcore/src/DatabaseContext/ApiDbContext.cs
@@ -85,11 +85,6 @@ namespace CSC.PublicApi.DatabaseContext
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost;User Id=ttvuser;Password=Tdsfkjds7632eDSG;database=Ttv_release_1_27;Encrypt=False");
-            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
