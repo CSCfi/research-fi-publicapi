3685c5f132048072e168d1089f9e7371b78e743b
Timo Särkikoski <sarkikos@gmail.com>
2024-03-27 15:12:52 +0200

    CSCTTV-3773 Check that database is in valid state before indexing

---
+++ aspnetcore/test/Indexer.Tests/Preflight/DatabasePreflightCheckTest.cs
@@ -0,0 +1,46 @@
+﻿
+using FluentAssertions;
+using Xunit;
+
+namespace CSC.PublicApi.Indexer.Tests.Preflight;
+
+public class DatabasePreflightCheckTest
+{
+    public DatabasePreflightCheckTest()
+    {
+    }
+
+    [Fact]
+    public void FactContributionNumberOfDistinctReferencesToDimPublicationIsGood_01()
+    {
+        // Arrange
+        DatabasePreflightCheck databasePreflightCheck = new DatabasePreflightCheck();
+        int dimPublicationCount = 10;
+        int factContributionDistinctReferencesToDimPublicationCount = 7;
+
+        // Act
+        bool actualResult = databasePreflightCheck.FactContributionNumberOfDistinctReferencesToDimPublicationIsGood(
+            dimPublicationCount,
+            factContributionDistinctReferencesToDimPublicationCount);
+
+        // Assert
+        Assert.False(actualResult);
+    }
+
+    [Fact]
+    public void FactContributionNumberOfDistinctReferencesToDimPublicationIsGood_02()
+    {
+        // Arrange
+        DatabasePreflightCheck databasePreflightCheck = new DatabasePreflightCheck();
+        int dimPublicationCount = 10;
+        int factContributionDistinctReferencesToDimPublicationCount = 9;
+
+        // Act
+        bool actualResult = databasePreflightCheck.FactContributionNumberOfDistinctReferencesToDimPublicationIsGood(
+            dimPublicationCount,
+            factContributionDistinctReferencesToDimPublicationCount);
+
+        // Assert
+        Assert.True(actualResult);
+    }
+}
