ef50a71a01f0170f21ff1b0f2b76321326f797e7
Timo Särkikoski <sarkikos@gmail.com>
2024-06-14 13:03:01 +0300

    CSCTTV-3882-exporter-console-app

---
+++ aspnetcore/src/Exporter/Exporters/FundingDecisionExporter.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Nest;
+using CSC.PublicApi.ElasticService;
+using CSC.PublicApi.Service.Models.FundingDecision;
+using FundingDecisionApiModel = ResearchFi.FundingDecision.FundingDecision;
+
+namespace BulkExport;
+
+public class FundingDecisionExporter
+{
+    private IElasticClient _elasticClient;
+    private readonly IMapper _mapper;
+    private readonly IndexNameSettings _indexNameSettings;
+
+
+    public FundingDecisionExporter(IElasticClient elasticClient, IMapper mapper, IndexNameSettings indexNameSettings)
+    {
+        _elasticClient = elasticClient;
+        _mapper = mapper;
+        _indexNameSettings = indexNameSettings;
+        
+    }
+
+    public void Export()
+    {   
+        Console.WriteLine("Start exporting funding decisions");
+        //string type = "CSC.PublicApi.Service.Models.FundingDecision.FundingDecision";
+        //var configuredTypesAndIndexNames = _indexNameSettings.GetTypesAndIndexNames();
+        var searchResponse = _elasticClient.Search<FundingDecision> (s => s.MatchAll().Index("api-dev-funding-decision"));
+        var docs = searchResponse.Documents;
+
+        foreach (var doc in docs)
+        {
+            FundingDecisionApiModel fdMapped = _mapper.Map<FundingDecisionApiModel>(doc);
+            Console.WriteLine($"data: {fdMapped.NameEn}");
+        }
+        
+        Console.WriteLine($"Done exporting funding calls: {searchResponse.Documents.Count}");
+    }
+}
---
+++ aspnetcore/src/Exporter/Program.cs
@@ -0,0 +1,25 @@
+﻿using BulkExport;
+using CSC.PublicApi.ElasticService;
+using CSC.PublicApi.Interface;
+using CSC.PublicApi.Interface.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) => {
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .AddEnvironmentVariables()
+            .Build();
+
+        services.AddSettings(configuration);
+        services.AddElasticSearch(configuration);
+        services.AddAutoMapper(typeof(ApiPolicies).Assembly);
+        services.AddTransient<IndexNameSettings>();
+        services.AddTransient<FundingDecisionExporter>();
+    })
+    .Build();
+
+var fundingDecisionExporter = host.Services.GetRequiredService<FundingDecisionExporter>();
+fundingDecisionExporter.Export();
