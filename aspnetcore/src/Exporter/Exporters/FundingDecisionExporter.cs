b2f464bf59e66978c003ba6bb2517fcd73666c92
Timo Särkikoski <sarkikos@gmail.com>
2024-06-20 15:39:41 +0300

    Working example of how an Elasticsearch item is converted into API model and written to json file

--- aspnetcore/src/Exporter/Exporters/FundingDecisionExporter.cs
+++ aspnetcore/src/Exporter/Exporters/FundingDecisionExporter.cs
@@ -3,6 +3,7 @@ using Nest;
 using CSC.PublicApi.ElasticService;
 using CSC.PublicApi.Service.Models.FundingDecision;
 using FundingDecisionApiModel = ResearchFi.FundingDecision.FundingDecision;
+using System.Text.Json;
 
 namespace BulkExport;
 
@@ -21,18 +22,24 @@ public class FundingDecisionExporter
         
     }
 
-    public void Export()
+    public void Export(JsonSerializerOptions serializerOptions)
     {   
         Console.WriteLine("Start exporting funding decisions");
         //string type = "CSC.PublicApi.Service.Models.FundingDecision.FundingDecision";
         //var configuredTypesAndIndexNames = _indexNameSettings.GetTypesAndIndexNames();
+
+        // First test using "select all", this needs to be converted to "search after" type search
         var searchResponse = _elasticClient.Search<FundingDecision> (s => s.MatchAll().Index("api-dev-funding-decision"));
         var docs = searchResponse.Documents;
 
         foreach (var doc in docs)
         {
-            FundingDecisionApiModel fdMapped = _mapper.Map<FundingDecisionApiModel>(doc);
-            Console.WriteLine($"data: {fdMapped.NameEn}");
+            FundingDecisionApiModel fundingDecision = _mapper.Map<FundingDecisionApiModel>(doc);
+
+            string jsonString = JsonSerializer.Serialize(fundingDecision, serializerOptions);
+            File.WriteAllText("/tmp/funding-decision-test-export.json", jsonString);
+
+            Console.WriteLine(jsonString);
         }
         
         Console.WriteLine($"Done exporting funding calls: {searchResponse.Documents.Count}");
--- aspnetcore/src/Exporter/Program.cs
+++ aspnetcore/src/Exporter/Program.cs
@@ -1,10 +1,25 @@
-﻿using BulkExport;
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using BulkExport;
 using CSC.PublicApi.ElasticService;
 using CSC.PublicApi.Interface;
 using CSC.PublicApi.Interface.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+// Define json serializer options
+// https://learn.microsoft.com/en-us/dotnet/api/system.text.json.jsonserializeroptions?view=net-6.0
+// https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/character-encoding
+// https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/customize-properties?pivots=dotnet-6-0
+// https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/ignore-properties#ignore-all-null-value-properties
+var jsonSerializerOptions = new JsonSerializerOptions {
+    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+    WriteIndented = true,
+};
 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) => {
@@ -22,4 +37,4 @@ var host = Host.CreateDefaultBuilder(args)
     .Build();
 
 var fundingDecisionExporter = host.Services.GetRequiredService<FundingDecisionExporter>();
-fundingDecisionExporter.Export();
+fundingDecisionExporter.Export(jsonSerializerOptions);
