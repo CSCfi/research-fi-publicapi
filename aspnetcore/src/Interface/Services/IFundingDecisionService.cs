723f4719d6eb05095d97387f6713ccc8ae1c9685
Timo Särkikoski <sarkikos@gmail.com>
2024-09-16 13:29:46 +0300

    Add search after functionality in funding decision endpoint. Improve index name settings unit test.

--- aspnetcore/src/Interface/Services/IFundingDecisionService.cs
+++ aspnetcore/src/Interface/Services/IFundingDecisionService.cs
@@ -7,4 +7,5 @@ namespace CSC.PublicApi.Interface.Services;
 public interface IFundingDecisionService
 {
     Task<(IEnumerable<FundingDecision>, SearchResult)> GetFundingDecisions(GetFundingDecisionQueryParameters fundingDecisionQueryParameters, PaginationQueryParameters paginationQueryParameters);
+    Task<(IEnumerable<FundingDecision>, long? searchAfter)> GetFundingDecisionsSearchAfter(GetFundingDecisionQueryParameters fundingDecisionQueryParameters, SearchAfterQueryParameters searchAfterQueryParameters);
 }
