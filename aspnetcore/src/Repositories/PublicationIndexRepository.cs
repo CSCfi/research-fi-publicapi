fedacf963eb4fce7e2259928c86c4673a78289e7
Timo Särkikoski <sarkikos@gmail.com>
2024-10-24 09:11:19 +0300

    CSCTTV-4013 prevent duplicates when collecting co-publication data

--- aspnetcore/src/Repositories/PublicationIndexRepository.cs
+++ aspnetcore/src/Repositories/PublicationIndexRepository.cs
@@ -84,38 +84,48 @@ public class PublicationIndexRepository : IndexRepositoryBase<Publication>
         // Add data from OrgPublication DTOs, which hold data collected from publications related to a co-publication (yhteisjulkaisu/osajulkaisu)
         if (publication.OrgPublicationDatabaseContributionDTOs != null && publication.OrgPublicationDatabaseContributionDTOs.Count > 0)
         {
+            List<FactContribution> databaseContributions = new();
             foreach (OrgPublicationDatabaseContributionDTO op in publication.OrgPublicationDatabaseContributionDTOs.AsEnumerable().ToList())
             {
-                publication.DatabaseContributions.AddRange(op.DatabaseContributions);
+                databaseContributions.AddRange(op.DatabaseContributions);
             }
+            publication.DatabaseContributions.AddRange(databaseContributions.DistinctBy(x => x.Name));
         }
         if (publication.OrgPublicationKeywordDTOs != null && publication.OrgPublicationKeywordDTOs.Count > 0)
         {
+            List<Keyword> keywords = new();
             foreach (OrgPublicationKeywordDTO kw in publication.OrgPublicationKeywordDTOs.AsEnumerable().ToList())
             {
-                publication.Keywords.AddRange(kw.Keywords);
+                keywords.AddRange(kw.Keywords);
             }
+            publication.Keywords.AddRange(keywords.DistinctBy(x => x.Value));
         }
         if (publication.OrgPublicationArtPublicatonTypeCategoryDTOs != null && publication.OrgPublicationArtPublicatonTypeCategoryDTOs.Count > 0)
         {
+            List<ReferenceData> artPublicationTypeCategories = new();
             foreach (OrgPublicationArtPublicatonTypeCategoryDTO ap in publication.OrgPublicationArtPublicatonTypeCategoryDTOs.AsEnumerable().ToList())
             {
-                publication.ArtPublicationTypeCategory.AddRange(ap.ArtPublicationTypeCategories);
+                artPublicationTypeCategories.AddRange(ap.ArtPublicationTypeCategories);
             }
+            publication.ArtPublicationTypeCategory.AddRange(artPublicationTypeCategories.DistinctBy(x => x.Code));
         }
         if (publication.OrgPublicationFieldsOfScienceDTOs != null && publication.OrgPublicationFieldsOfScienceDTOs.Count > 0)
         {
+            List<ReferenceData> fieldsOfScience = new();
             foreach (OrgPublicationFieldsOfScienceDTO fs in publication.OrgPublicationFieldsOfScienceDTOs.AsEnumerable().ToList())
             {
-                publication.FieldsOfScience.AddRange(fs.FieldsOfScience);
+                fieldsOfScience.AddRange(fs.FieldsOfScience);
             }
+            publication.FieldsOfScience.AddRange(fieldsOfScience.DistinctBy(x => x.Code));
         }
         if (publication.OrgPublicationFieldsOfArtDTOs != null && publication.OrgPublicationFieldsOfArtDTOs.Count > 0)
         {
+            List<ReferenceData> fieldsOfArt = new();
             foreach (OrgPublicationFieldsOfArtDTO fa in publication.OrgPublicationFieldsOfArtDTOs.AsEnumerable().ToList())
             {
-                publication.FieldsOfArt.AddRange(fa.FieldsOfArt);
+                fieldsOfArt.AddRange(fa.FieldsOfArt);
             }
+            publication.FieldsOfArt.AddRange(fieldsOfArt.DistinctBy(x => x.Code));
         }
          
         HandleIssnAndIsbn(publication);
