196be8e777786de8afed317bddc2c0ae1bf92c1f
LauriHellsten <lauri.hellsten@csc.fi>
2025-02-19 14:09:22 +0200

    Cscttv 3834 virta report apis
    Virta report apis added to TTV publicAPI

---
+++ aspnetcore/src/ApiModels/Query/GetVirtaQueryParameters.cs
@@ -0,0 +1,12 @@
+﻿namespace ResearchFi.Query;
+
+/// <summary>
+/// Hakuparametrit organisaatioiden hakemiseen.
+/// </summary>
+public class GetVirtaQueryParameters 
+{
+    /// <summary>
+    /// Organisaation tunnus. Oppilaitosnumero tai virastotunnus. Jos organisaatiolla ei ole edellämainittuja, käytä y-tunnusta ilman väliviivaa (y-tunnus 1234567-1 muodossa 12345671)
+    /// </summary>
+    public string? organisaatiotunnus { get; set; }
+}
---
+++ aspnetcore/src/Interface/Models/Duplikaatit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSC.PublicApi.Interface.Models
+{
+    public partial class Duplikaatit
+    {
+        public long DuplikaattiId { get; set; }
+        public string Organisaatiotunnus { get; set; } = null!;
+        public string? Organisaationimi { get; set; }
+        public string Julkaisuntunnus { get; set; } = null!;
+        public string? Kuvaus { get; set; }
+        public string Julkaisunorgtunnus { get; set; } = null!;
+        public string Duplikaattijulkaisunorgtunnus { get; set; } = null!;
+        public string? Julkaisunnimi { get; set; }
+        public string? Duplikaattijulkaisunnimi { get; set; }
+        public string? Julkaisutyyppikoodi { get; set; }
+        public int? Tila { get; set; }
+        public long TarkistusId { get; set; }
+        public DateTime? Luontipaivamaara { get; set; }
+        public int? Ilmoitusvuosi { get; set; }
+        public int? Julkaisuvuosi { get; set; }
+    }
+}
