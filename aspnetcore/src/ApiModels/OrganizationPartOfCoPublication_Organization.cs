31bfb47dfaec46069129a7159635637a2f86f857
Timo Särkikoski <sarkikos@gmail.com>
2025-02-27 14:45:20 +0200

    CSCTTV-4139 modify orgPublication handling

---
+++ aspnetcore/src/ApiModels/OrganizationPartOfCoPublication_Organization.cs
@@ -0,0 +1,10 @@
+namespace ResearchFi;
+
+/// <summary> 
+/// </summary>
+public class OrganizationPartOfCoPublication_Organization
+{
+    /// <summary>
+    /// </summary>
+    public string? Id { get; set; }
+}
