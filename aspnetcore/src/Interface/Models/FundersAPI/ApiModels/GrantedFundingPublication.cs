ef0f28374b35ebb6e93d5c84d832b48f1e1459c4
Timo Särkikoski <sarkikos@gmail.com>
2025-05-12 15:03:04 +0300

    Add Import DB context and models. Add funders controller and endpoints.

---
+++ aspnetcore/src/Interface/Models/FundersAPI/ApiModels/GrantedFundingPublication.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSC.PublicApi.Interface.Models.FundersAPI.ApiModels
+{
+    public partial class GrantedFundingPublication
+    {
+        [Required(ErrorMessage = "Either PublicationId or Doi must be provided.")]
+        public string? PublicationId { get; set; }
+
+        [Required(ErrorMessage = "Either PublicationId or Doi must be provided.")]
+        public string? Doi { get; set; }
+
+        public bool IsValid()
+        {
+            return !string.IsNullOrEmpty(PublicationId) || !string.IsNullOrEmpty(Doi);
+        }
+
+        public string GetPublicationIdentifier()
+        {
+            if (!string.IsNullOrEmpty(PublicationId))
+            {
+                return PublicationId;
+            }
+            else if (!string.IsNullOrEmpty(Doi))
+            {
+                return Doi;
+            }
+            else
+            {
+                throw new InvalidOperationException("Neither PublicationId nor Doi is set.");
+            }
+        }
+    }
+}
