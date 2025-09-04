3e0a50ad8781df7d2f094bc6f3c70796a0d48607
Timo Särkikoski <sarkikos@gmail.com>
2025-09-04 09:06:41 +0300

    Remove development connection string

--- aspnetcore/src/DatabaseContext/ApiDbContext.cs
+++ aspnetcore/src/DatabaseContext/ApiDbContext.cs
@@ -160,10 +160,6 @@ public partial class ApiDbContext : DbContext
 
     public virtual DbSet<FactWordClusterToDomain> FactWordClusterToDomains { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;User Id=ttvuser;Password=Tdsfkjds7632eDSG;database=Ttv_2025_08_27;Encrypt=False");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<BrDatasetDatasetRelationship>(entity =>
