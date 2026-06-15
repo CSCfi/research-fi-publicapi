6a2cfbc78ebb86279e1f1997136a6330db7a09cb
Timo Särkikoski <sarkikos@gmail.com>
2026-06-15 14:22:07 +0300

    Update InfrastructureProfile to filter InfraRelations and uncomment Pid collection

--- aspnetcore/src/Repositories/Maps/InfrastructureProfile.cs
+++ aspnetcore/src/Repositories/Maps/InfrastructureProfile.cs
@@ -69,7 +69,7 @@ public class InfrastructureProfile : Profile
             // Infrastructure network
             // Collect only from "FactRelationFromInfrastructures", do not collect "FactRelationToInfrastructures"
             .ForMember(dst => dst.InfraRelations,
-                opt => opt.MapFrom(src => src.FactRelationFromInfrastructures
+                opt => opt.MapFrom(src => src.FactRelationFromInfrastructures.Where(rel => rel.ToInfrastructureId != -1)
                     .Select(rel => new InfrastructureNetwork
                     {
                         RelationType = rel.RelationTypeCodeNavigation != null
@@ -85,12 +85,12 @@ public class InfrastructureProfile : Profile
                             }
                             : null,
                         RelationValid = rel.ValidRelation,
-                        // Pids = rel.ToInfrastructure.DimPids // Collect Pids, processed in InfrastructureIndexRepository
-                        //     .Select(dp => new PersistentIdentifier
-                        //     {
-                        //         Content = dp.PidContent,
-                        //         Type = dp.PidType
-                        //     }).ToList(),
+                        Pids = rel.ToInfrastructure.DimPids // Collect Pids, processed in InfrastructureIndexRepository
+                            .Select(dp => new PersistentIdentifier
+                            {
+                                Content = dp.PidContent,
+                                Type = dp.PidType
+                            }).ToList(),
                         RelationToNationalInfra = null, // Handled in InfrastructureIndexRepository
                         RelationToInternationalInfra = null // TODO: Handled in InfrastructureIndexRepository
                     }).ToList()
